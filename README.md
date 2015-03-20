02d04530329bfdfa0ade46c5ba1563e2ac634260
William Jimenez <william.jimenez@itsoninc.com>
2015-03-20 14:30:53 -0700

    adding first working version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # elasticsearch-web-export
 Export Elasticsearch data using an HTTP/web based interface
+
+This is a very basic interface to elasticsearch that can be used in a web browser for users who are less 
+comfortable with the command line (and who are likely already in a browser based interface using Kibana). 
+It has a lot of room for improvement to make it more user-friendly.
+
+## Requirements 
+- flask
+- stash-query, installed in the user's path: https://github.com/robbydyer/stash-query 
---
+++ main.py
@@ -0,0 +1,74 @@
+#
+# elasticsearch-web-export
+# Author: William Jimenez <wjimenez5271@gmail.com>
+# License: GNU GPL 2.0
+# Requires stash-query to do handle ElasticSearch interface: https://github.com/robbydyer/stash-query
+#
+
+from flask import Flask, make_response, request, url_for
+import subprocess
+import os
+
+if not os.path.isfile('/usr/bin/stash-query'):
+    raise Exception('Please ensure you have stash-query installed: https://github.com/robbydyer/stash-query')
+
+app = Flask(__name__)
+
+
+def do_query(start_time, end_time, query, output_file='/tmp/es-output.txt'):
+    """
+    Execute stash-query with parameters, and return results as string object
+    :param start_time: Start time of query: e.g. 2015-03-20T10:36:47.621Z
+    :param end_time: End time of query
+    :param query: Query parameters, e.g. 'type:"saas_access"'
+    :param output_file: tmp location for stash-query to write output to. We'll read the data from this location
+    :return: str. results of query
+    """
+    cmd = 'stash-query -s {0} -e {1} -q {2} -w {3}'.format(start_time, end_time, query, output_file)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+    output, errors = p.communicate()
+    if p.returncode or errors:
+        # Print for debugging purposes to console
+        print('Request Parameters: \n')
+        print(start_time)
+        print(end_time)
+        print(query)
+        print(errors)
+        print(output)
+    results = open(output_file, 'r')
+    results_data = results.read()
+    results.close()
+    os.remove(output_file)
+    return results_data
+
+
+# Main interface to web service
+@app.route('/')
+def index():
+
+    return '<b>ELK Data Export</b><p>' \
+           'Hello. To construct a query, you\'ll need to create a URL like so: <p>' \
+           '<pre>' \
+           '[hostname:port]/query?start_time=2015-03-20T10:36:47.621Z&end_time=2015-03-20T10:40:47.621Z&query=type:"apache"' \
+           '</pre>'
+
+# Query interface
+@app.route('/query')
+def query():
+    # Format response payload
+    if len(request.args) == 0:
+        return 'Error: Missing URL Parameters'
+    response = make_response(do_query(request.args.get('start_time'),
+                                      request.args.get('end_time'),
+                                      request.args.get('query')))
+    # Set headers
+    response.headers["Content-Disposition"] = "attachment; filename=query_results.txt"
+    return response
+
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=int("8085"),
+        debug=True
+    )
